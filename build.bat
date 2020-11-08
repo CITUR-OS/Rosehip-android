45be7855af202aaa74474d126f23f9bdfbcbdf57
Elisha Hollander <just4now666666@gmail.com>
2020-11-08 23:59:39 +0200

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,10 @@
+echo key.alias=android>local.properties
+echo key.store.password=android>>local.properties
+echo key.alias.password=android>>local.properties
+echo key.store=android.keystore>>local.properties
+set sdk=sdk.dir=%~dp0android-sdk
+echo %sdk:\=\\%>>local.properties
+apache-ant\\bin\\ant.bat clean release install
+android-sdk\\platform-tools\\adb.exe push main.1.com.Rosehip.myapp.obb /mnt/sdcard/main.1.com.Rosehip.myapp.obb
+move main.1.com.Rosehip.myapp.obb bin/
+android-sdk\\platform-tools\\adb.exe shell am start -W -a android.intent.action.MAIN com.Rosehip.myapp/org.renpy.android.PythonActivity
