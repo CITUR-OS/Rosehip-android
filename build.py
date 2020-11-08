ffe8ecbdb8350bd1c6173fb932332aebb61f4e47
Elisha Hollander <just4now666666@gmail.com>
2020-11-08 23:50:02 +0200

    typo

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 from subprocess import check_call
 from os import rename, path
-open('local.properties','a').write('key.alias=android\nkey.store.password=android\ney.alias.password=android\nkey.store=android.keystore\nsdk.dir=' + path.dirname(path.abspath(__file__)).replace('\\','\\\\') + '\\\\android-sdk')
+open('local.properties','a').write('key.alias=android\nkey.store.password=android\nkey.alias.password=android\nkey.store=android.keystore\nsdk.dir=' + path.dirname(path.abspath(__file__)).replace('\\','\\\\') + '\\\\android-sdk')
 check_call(["apache-ant\\bin\\ant.bat", "clean", "release", "install"])
 check_call(["android-sdk\\platform-tools\\adb.exe", "push", "main.1.com.Rosehip.myapp.obb", "/mnt/sdcard/main.1.com.Rosehip.myapp.obb"])
 rename("main.1.com.Rosehip.myapp.obb", "bin/main.1.com.Rosehip.myapp.obb")
